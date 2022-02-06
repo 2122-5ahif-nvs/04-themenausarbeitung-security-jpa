17bc8c26cd0f53320632bc5a1a9c98c7a339459e
Muhammet190 <m.oezdogan@students.htl-leonding.ac.at>
2022-02-06 20:46:34 +0100

    code example finished

---
+++ security-jpa/src/main/java/at/htl/boundary/PublicResource.java
@@ -0,0 +1,20 @@
+package at.htl.boundary;
+
+import javax.annotation.security.PermitAll;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/api/public")
+public class PublicResource {
+
+
+    @GET
+    @PermitAll
+    @Produces(MediaType.TEXT_PLAIN)
+    public String publicResource() {
+        return "public";
+    }
+}
